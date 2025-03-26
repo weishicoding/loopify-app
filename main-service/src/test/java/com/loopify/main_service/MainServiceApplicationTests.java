2451fa7581158a6d0080af8a337f0fd27daac7cd
weishicoding <sanwind0305@gmail.com>
2025-03-26 22:31:45 +0200

    first commit -environment configuration

---
+++ main-service/src/test/java/com/loopify/main_service/MainServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.loopify.main_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
