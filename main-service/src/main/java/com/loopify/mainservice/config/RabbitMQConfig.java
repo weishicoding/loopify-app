bb8522b0ca8a32fb5d8572c58d5f75f4a088979b
weishicoding <sanwind0305@gmail.com>
2025-04-11 09:31:32 +0300

    Modify docker configuration to make sure that rabbitmq connect correctly

--- main-service/src/main/java/com/loopify/mainservice/config/RabbitMQConfig.java
+++ main-service/src/main/java/com/loopify/mainservice/config/RabbitMQConfig.java
@@ -1,11 +1,9 @@
 package com.loopify.mainservice.config;
 
-import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
